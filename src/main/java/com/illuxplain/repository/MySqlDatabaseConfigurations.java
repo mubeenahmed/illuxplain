ec52827139eec859e1628436a248f4948a72f232
Mubeen Ahmed <mubeenahmedsiddiqui@gmail.com>
2016-06-26 01:26:12 +0500

    fixed

--- src/main/java/com/illuxplain/repository/MySqlDatabaseConfigurations.java
+++ src/main/java/com/illuxplain/repository/MySqlDatabaseConfigurations.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class MySqlDatabaseConfigurations {
 	//jdbc:mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/burgerjoint
 	private static Connection connection;
-	private static final String username = "adminR5nMvhl";
-	private static final String password = "yD_7aXK4uBtg";
+	private static final String username = "adminWj3LtJS";
+	private static final String password = "jmMLPhqVhMra";
 	//private static final String host =  System.getenv("OPENSHIFT_MYSQL_DB_HOST");
 	//private static final String port =  System.getenv("OPENSHIFT_MYSQL_DB_PORT");
 	//private static final String URL = "jdbc:mysql://"+host+":"+port+"/illuxplain";
