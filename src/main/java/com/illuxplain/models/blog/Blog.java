cd4ca6e360f0ebeef0eeab6f4f854555c0a6bfaf
= <=>
2018-02-17 20:17:38 +0500

    fixing some bugs

--- src/main/java/com/illuxplain/models/blog/Blog.java
+++ src/main/java/com/illuxplain/models/blog/Blog.java
@@ -17,6 +17,8 @@ public class Blog {
     @Column(name = "title_content")
     private String titleContent;
 
+    @Lob
+    @Column( length = 100000 )
     private String content;
 
     @ManyToOne
